5134c918b670e0c74d5fabc05a37c9d32230b569
PhyrexianForces <riley.bacigalupi@gmail.com>
2026-03-31 10:18:09 -0400

    Create ModItems.java

---
+++ src/main/java/net/VoidRunner0/nyctophobia/ModItems.java
@@ -0,0 +1,17 @@
+package net.VoidRunner0.nyctophobia.item;
+
+import net.VoidRunner0.nyctophobia.Nyctophobia;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItems {
+  public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, Nyctophobia.MOD_ID);
+
+
+  public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+}
